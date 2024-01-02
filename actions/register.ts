cf2d8231a35caa6f1ca4aab837613fa3e648ece9
ReDay Zarra <redayzarra@gmail.com>
2024-01-02 05:43:37 -0500

    Database is working and able to add users with registerForm

--- actions/register.ts
+++ actions/register.ts
@@ -2,13 +2,40 @@
 
 import { registerSchema } from "@/schemas";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import db from "@/lib/db";
 
 export const register = async (values: z.infer<typeof registerSchema>) => {
+  // Parse the values through our registerSchema
   const validated = registerSchema.safeParse(values);
 
-  if (!validated) {
+  // If we don't have a safe parse, throw an error immediately
+  if (!validated.success) {
     return { error: "Invalid information provided!" };
   }
 
-  return { success: "Confirmation email has been sent!" };
+  // Extract the values from validated
+  const { username, email, password } = validated.data;
+
+  // Hash our password using bcrypt
+  const hashedPassword = await bcrypt.hash(password, 0);
+
+  // Confirm that the email is not taken
+  const existingUser = await db.user.findUnique({
+    where: { email },
+  });
+
+  if (existingUser) {
+    return { error: "Email is already is use" };
+  }
+
+  await db.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return { success: "User created!" };
 };
